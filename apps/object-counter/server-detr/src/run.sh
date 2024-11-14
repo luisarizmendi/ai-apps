9bdb8142e34823ee2517e15693743936e9115c22
Luis Arizmendi <luis.ariz@gmail.com>
2024-11-14 18:24:52 +0100

    remove model path

--- apps/object-counter/server-detr/src/run.sh
+++ apps/object-counter/server-detr/src/run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ ${MODEL_PATH} ]; then
-    PORT=${PORT} MODEL_PATH=${MODEL_PATH} uvicorn object_detection_server:app --port ${PORT:=8000} --host ${HOST:=0.0.0.0}
-    exit 0
-fi
+uvicorn object_detection_server:app --port ${PORT:=8000} --host ${HOST:=0.0.0.0}
 
-echo "Please set either a MODEL_PATH"
-exit 1
+
